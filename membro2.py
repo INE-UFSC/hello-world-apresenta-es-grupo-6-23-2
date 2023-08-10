a964afab7e28cbf4c6a839ff3262258a743b7806
J. Sutter <joaovsutter21@gmail.com>
2023-08-10 14:53:21 -0300

    Apresentação João

--- membro2.py
+++ membro2.py
@@ -1,2 +1,6 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	string = 'Olá, meu nome é João Sutter'
+	string += 'Tenho 20 anos, e no meu tempo livre gosto de ler, escutar música e assistir filmes'
+	string += 'Minha experiência prévia com programação foi no curso de Eletrônica do IFSC, onde aprendi a linguagem C'
+	string += 'Espero que esta disciplina contribua para minha formação pessoal como cientista da computação'
+	return string #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
